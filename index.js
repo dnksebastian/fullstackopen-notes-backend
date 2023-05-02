d403d9d990b70e530115e74c1344ce6d93a187b0
dnksebastian <sebastian.dulniawka@gmail.com>
2023-05-02 22:38:46 +0200

    notes backend db connect

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 
 const Note = require('./models/note')
 
@@ -32,14 +32,27 @@ let notes = [
     })
   })
 
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
+    }
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
+  
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
+  })
+
   app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()}
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -47,33 +60,6 @@ let notes = [
     notes = notes.filter(note => note.id !== id)
   
     response.status(204).end()
-  })
-
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
-  
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
   })
 
 app.use(unknownEndpoint)
