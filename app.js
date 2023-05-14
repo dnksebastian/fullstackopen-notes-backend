3655d5237aa735c6fd306418b3c2362c2132101e
dnksebastian <sebastian.dulniawka@gmail.com>
2023-05-14 21:54:59 +0200

    notes backend token authentication

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 require('express-async-errors')
 
+const loginRouter = require('./controllers/login')
 const usersRouter = require('./controllers/users')
 const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
@@ -27,6 +28,7 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter)
 
